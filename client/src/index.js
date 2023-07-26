aed4308976a864dc9aacdb35d0bf139a4d0e8d32
Mana <managhabraei2022@gmail.com>
2023-07-26 17:37:04 +0100

    correct the import part

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "../src/styles/index.css";
+import "./styles/index.css";
 
 
 import App from "./App";
