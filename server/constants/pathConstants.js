33c12369675102dc01f676e946e60b9852a5e73b
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-27 17:33:56 +0100

    updated pathConstants.js

--- server/constants/pathConstants.js
+++ server/constants/pathConstants.js
@@ -1,1 +1,8 @@
-export const GET_USERS_PATH = "/get-users";
+const GET_USERS_PATH = "/get-users";
+
+module.exports = {
+	GET_USERS_PATH,
+};
+
+// eslint-disable-next-line no-console
+console.log(GET_USERS_PATH);
