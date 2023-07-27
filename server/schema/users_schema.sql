726117bb4d5fc65ba136fc7aebfa12422d712e61
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-07-28 00:27:44 +0100

    added default values

--- server/schema/users_schema.sql
+++ server/schema/users_schema.sql
@@ -1,22 +1,21 @@
 DROP TABLE IF EXISTS users;
 
-
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
     role VARCHAR(50) NOT NULL,
-    nationality VARCHAR(50),
+    nationality VARCHAR(50) DEFAULT 'Unknown',
     location VARCHAR(100) NOT NULL,
-    soft_skills VARCHAR(200),
-    hard_skills VARCHAR(200),
-    mobile VARCHAR(20),
+    soft_skills VARCHAR(200) DEFAULT 'Not specified',
+    hard_skills VARCHAR(200) DEFAULT 'Not specified',
+    mobile VARCHAR(20) DEFAULT 'N/A',
     email VARCHAR(100) NOT NULL,
-    linkedin VARCHAR(100),
-    youtube VARCHAR(100),
-    description VARCHAR(500) NOT NULL,
-    hobbies VARCHAR(200)
+    linkedin VARCHAR(100) DEFAULT 'Not specified',
+    youtube VARCHAR(100) DEFAULT 'Not specified',
+    description VARCHAR(255) NOT NULL,
+    hobbies VARCHAR(200) DEFAULT 'None'
 );
 
 
