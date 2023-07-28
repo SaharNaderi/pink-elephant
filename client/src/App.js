5711fb61d248dec2bf09b2992028211ea2670892
Mana <managhabraei2022@gmail.com>
2023-07-28 17:47:25 +0100

    fix-router structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,15 @@ import PageTemplate from "./template/PageTemplate";
 import LandingPage from "./template/LandingPage";
 
 
-import About from "./pages/About";
-import Home from "./pages/Home";
 
 const App = () => (
 	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/" element={<PageTemplate />} />
-		<Route path ="landingPage" element={<LandingPage />} />
-	
+	<Route path="/" element={<PageTemplate />}>
+		<Route index element={<LandingPage />}></Route>
+	</Route>
+
+
+
 
 	</Routes>
 );
