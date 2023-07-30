06a9105fccf4350b0df9d07cfce11cd8e2b44279
Mana <managhabraei2022@gmail.com>
2023-07-30 13:36:06 +0100

    feat/HamburgerMenu

---
+++ client/src/components/HumberMenu.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function HumberMenu() {
+  return (
+    <div>
+      <svg
+        width="60"
+        height="60"
+        viewBox="0 0 60 60"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M12.5 30H47.5H12.5Z"
+          fill="white"
+        />
+        <path
+          d="M12.5 30H47.5"
+          stroke="#EFC74E"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M12.5 17.5H47.5H12.5Z"
+          fill="white"
+        />
+        <path
+          d="M12.5 17.5H47.5"
+          stroke="#EFC74E"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M12.5 42.5H47.5H12.5Z"
+          fill="white"
+        />
+        <path
+          d="M12.5 42.5H47.5"
+          stroke="#EFC74E"
+          stroke-width="1.5"
+          stroke-linecap="round"
+        />
+      </svg>
+    </div>
+  );
+}
