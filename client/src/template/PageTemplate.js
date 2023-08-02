7f18bb81d18f10ff5dc66d5ea48849e12d2a547d
Mana <managhabraei2022@gmail.com>
2023-08-02 20:29:31 +0100

    fix-responsive-template

---
+++ client/src/template/PageTemplate.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./../Styles/pagetemplate.css";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
+import { Outlet } from "react-router-dom";
+
+export default function PageTemplate() {
+    return (
+        <article className="template-body">
+
+            <Header></Header>
+
+            <main>
+                <Outlet></Outlet>
+            </main>
+
+            <Footer></Footer>
+        </article>
+    );
+}
