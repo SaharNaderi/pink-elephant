42f6acae49929169b0467836df5d712e6ecb2c7e
sahar <sahar.nnad@gmail.com>
2023-08-03 13:25:48 +0100

    feat- updated

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,6 +5,7 @@ const About = () => (
 			<p>
 				Starter kit for full-stack JavaScript projects. For more information,
 				see the wiki:
+				
 			</p>
 			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
 		</div>
